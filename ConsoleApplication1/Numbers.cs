2097fa1c68881c09c9e49cb7193b501b0aa9b2d4
Vinzzztty <kevinarnandes21@students.amikom.ac.id>
2022-04-06 12:12:29 +0700

    first commit

---
+++ ConsoleApplication1/Numbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number2;
+            else if (number1 < number2)
+                return number1;
+            return 0;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            if (number1 < number2 && number1 < (int)number3)
+                return number1;
+            else if (number2 < number1 && number2 < (int)number3)
+                return number2;
+            else if ((int)number3 < number1 && (int)number3 < number2)
+                return (int)number3;
+
+            return 0;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number1;
+            else if (number1 < number2)
+                return number2;
+            return 0;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            if (number1 > number2 && number1 > (int)number3)
+                return number1;
+            else if (number2 > number1 && number2 > (int)number3)
+                return number2;
+            else if ((int)number3 > number1 && (int)number3 > number2)
+                return (int)number3;
+
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine("Angka Minimum #1: {0}", numbers.FindMinimum(3,1));
+            Console.WriteLine("Angka Minimum #2: {0}", numbers.FindMinimum(3, 2, 4));
+            Console.WriteLine(); //baris kosong
+            Console.WriteLine("Angka Maximum #1: {0}", numbers.FindMaximum(3, 1));
+            Console.WriteLine("Angka Maximum #2: {0}", numbers.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
